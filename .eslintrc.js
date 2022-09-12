462e100755770cff46ed23ac59fd1153b5ae748d
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-09-12 18:09:55 -0500

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,57 @@
 module.exports = {
-    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-    extends: [
-        "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-        "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
-        "plugin:jsx-a11y/recommended", // Uses the recommended rules from jsx-a11y for accessibility
-        "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "plugin:jsx-a11y/recommended", // Uses the recommended rules from jsx-a11y for accessibility
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  plugins: ["@typescript-eslint", "react", "jsx-a11y"],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    quotes: [1, "double"],
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        prefix: ["I"],
+      },
     ],
-    plugins: ["@typescript-eslint", "react", "jsx-a11y"],
-    parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: "module", // Allows for the use of imports
-        ecmaFeatures: {
-            jsx: true, // Allows for the parsing of JSX
+    "prettier/prettier": "warn",
+    "require-jsdoc": [
+      "warn",
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true,
         },
-    },
-    rules: {
-        quotes: [1, "double"],
-        "@typescript-eslint/naming-convention": [
-            "warn",
-            {
-                selector: "interface",
-                format: ["PascalCase"],
-                prefix: ["I"],
-            },
-        ],
-        "prettier/prettier": "warn",
-        "require-jsdoc": [
-            "warn",
-            {
-                require: {
-                    FunctionDeclaration: true,
-                    MethodDefinition: true,
-                    ClassDeclaration: true,
-                    ArrowFunctionExpression: true,
-                    FunctionExpression: true,
-                },
-            },
-        ],
-        "valid-jsdoc": "warn",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/ban-types": "warn",
-    },
-    settings: {
-        react: {
-            version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
-        },
-    },
-    overrides: [
-        {
-            files: ["**/*.tsx"],
-            rules: {
-                "react/prop-types": "off",
-            },
-        },
+      },
     ],
+    "valid-jsdoc": "warn",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/ban-types": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  overrides: [
+    {
+      files: ["**/*.tsx"],
+      rules: {
+        "react/prop-types": "off",
+      },
+    },
+  ],
 };
