638f67bfa07694d9c28bfdecc322f02dd898db69
Juan Sebastian Ramirez <juanramirezcastaneda@gmail.com>
2022-10-05 18:42:40 -0500

    configure default saga

--- src/Components/AuthorsContainer.tsx
+++ src/Components/AuthorsContainer.tsx
@@ -1,13 +1,25 @@
 import { connect } from "react-redux";
 
 import { IAppState } from "../Reducers/IAppState";
+import * as AuthorsAction from "../Reducers/Authors/AuthorsAction";
+import { Dispatch } from "@reduxjs/toolkit";
 
 interface IAuthorsContainerProps {
   repoOwner: string;
+  callbacks: {
+    onGetRepoOwnerClick: () => void;
+  };
 }
 
 function AuthorsContainer(props: IAuthorsContainerProps) {
-  return <span>{props.repoOwner}</span>;
+  const { repoOwner, callbacks } = props;
+
+  return (
+    <div>
+      <span>{repoOwner}</span>
+      <button onClick={callbacks.onGetRepoOwnerClick}>Get Repo Owner!</button>
+    </div>
+  );
 }
 
 const mapStateToProps = (state: IAppState) => {
@@ -16,4 +28,13 @@ const mapStateToProps = (state: IAppState) => {
   };
 };
 
-export default connect(mapStateToProps)(AuthorsContainer);
+const mapDispatchToProps = (dispatch: Dispatch<AuthorsAction.IGetRepoOwnerData>) => {
+  return {
+    callbacks: {
+      onGetRepoOwnerClick: () => {
+        dispatch(AuthorsAction.getRepoOwnerData());
+      },
+    },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(AuthorsContainer);
--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,17 +1,23 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import "regenerator-runtime/runtime";
 
 import authors from "../Reducers/Authors";
+import authorsSaga from "../Reducers/Authors/AuthorsSaga";
 import createRouter from "../Reducers/Router";
 import { IAppState } from "../Reducers/IAppState";
 
 export function createStore() {
   const router = createRouter();
+  const sagaMiddleware = createSagaMiddleware();
 
   const rootReducer = combineReducers<IAppState>({
     authors,
     router,
   });
 
-  return configureStore({ reducer: rootReducer, middleware: [logger] });
+  const store = configureStore({ reducer: rootReducer, middleware: [logger, sagaMiddleware] });
+  sagaMiddleware.run(authorsSaga);
+  return store;
 }
